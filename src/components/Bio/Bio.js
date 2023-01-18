6362d7f5c81aa2e203b3dcea4e5c92295874a45e
Aya Seidemann <aya.seidemann@gmail.com>
2023-01-17 23:08:32 -0800

    bio format and styling

--- src/components/Bio/Bio.js
+++ src/components/Bio/Bio.js
@@ -6,8 +6,13 @@ function Bio() {
     return (
         <div className='bio'>
             <div className='bio__text'>
-                <h2 className='bio__header'>Hello!</h2>
-                <p className='bio__p'>Here's my bio, I'm a pretty cool person</p>
+                <div className='bio__header--wrapper'>
+                    <h2 className='bio__header'>Hello!</h2>
+                </div>
+                <h3 className='bio__subheader'>A positive and focused Software Developer with years of management work experience and a math background. </h3>
+                <p className='bio__p'>
+                    I have always found joy and satisfaction in logic, coding, and collaboration, so transitioning to software development just made sense. I earned two Bachelor Degrees (B.S. and B.A.) from The University of Texas, double-majoring in Math and Theatre. I loved the juxtaposition of math and art -- math and science provided joy in working out concrete solutions, while art provided freedom due to the absence of such concrete solutions. After university, I began my career as a software implementation Project Manager at Epic, and found myself increasingly intrigued by the intricacies of developing effective software that solves problems. I also grew my analytical and communication skills, which I actively bring into the software development process. Combining attention to detail, problem-solving, and organizational skills gained from coordinating a non-profit tutoring center, I have become a driven and nuanced Developer.
+                </p>
             </div>
             <img className='bio__pic' src={profilePic} alt='headshot'/>
         </div>
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,5 +1,6 @@
 import './Header.scss';
 import { Link } from 'react-router-dom';
+import { Moon, Sun, EnvelopeSimple, GithubLogo, LinkedinLogo } from "phosphor-react";
 
 function Header() {
 
@@ -10,10 +11,18 @@ function Header() {
                     <h1 className='header__title'>Aya Seidemann</h1>
                 </Link>
                 <div className='header__right'>
-                    <button className='header__icon header__icon--mode'>🌙</button>
-                    <a className='header__icon header__icon--email' href="mailto:aya.seidemann@gmail.com">EM</a>
-                    <a className='header__icon header__icon--github' href='https://github.com/ayaseidemann'>GH</a>
-                    <a className='header__icon header__icon--linkedin' href='https://www.linkedin.com/in/aya-seidemann/'>LI</a>
+                    <button className='header__icon header__icon--mode'>
+                        <Moon size={24} />
+                    </button>
+                    <a className='header__icon header__icon--email' href="mailto:aya.seidemann@gmail.com">
+                        <EnvelopeSimple size={24} />
+                    </a>
+                    <a className='header__icon header__icon--github' href='https://github.com/ayaseidemann'>
+                        <GithubLogo size={24} />
+                    </a>
+                    <a className='header__icon header__icon--linkedin' href='https://www.linkedin.com/in/aya-seidemann/'>
+                        <LinkedinLogo size={24} />
+                    </a>
                 </div>
             </header>
         </div>
