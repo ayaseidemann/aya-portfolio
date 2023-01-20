2114ea2628334d06493b8a23ca52e863ed9dd391
Aya Seidemann <aya.seidemann@gmail.com>
2023-01-20 15:40:05 -0800

    add mockup images and more projects

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,7 +1,8 @@
 import './Projects.scss';
-import profilePic from '../../assets/Pictures/aya-full.png';
 import sudotwoLaptop from '../../assets/Pictures/sudotwo-laptop.png';
 import sudotwoPhone from '../../assets/Pictures/sudotwo-phone.png';
+import telusLaptop from '../../assets/Pictures/telus-laptop.png';
+import telusPhone from '../../assets/Pictures/telus-phone.png';
 
 
 function Projects() {
@@ -32,16 +33,19 @@ function Projects() {
                     </div>
                 </div>
                 <div className='card'>
-                    {/* <img className='card__img' src={sudotwo} alt='project 2'/> */}
+                    <div className='card__images'>
+                        <img className='card__phone' src={telusPhone} alt='telus'/>
+                        <img className='card__laptop' src={telusLaptop} alt='telus'/>
+                    </div>
                     <div className='card__text'>
-                        <h3 className='card__title'>Project 2</h3>
+                        <h3 className='card__title'>Telus Hackathon</h3>
                         <div className='card__tags'>
                             <span className='card__tag'>JavaScript</span>
+                            <span className='card__tag'>React</span>
                             <span className='card__tag'>HTML</span>
                             <span className='card__tag'>CSS</span>
-                            <span className='card__tag'>Express</span>
                         </div>
-                        <p className='card__p'>Here's some info about the project</p>
+                        <p className='card__p'>I co-developed a new TV channel bundling site for Telus in a hackathon</p>
                     </div>
                 </div>
                 <div className='card'>
