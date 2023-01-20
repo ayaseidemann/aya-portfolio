7b0cd4ac57c7a106374c9707d46e95f76e8f6e3e
Aya Seidemann <aya.seidemann@gmail.com>
2023-01-20 12:19:48 -0800

    update colors and overall styling, add pictures

--- src/components/Bio/Bio.js
+++ src/components/Bio/Bio.js
@@ -1,5 +1,5 @@
 import './Bio.scss';
-import profilePic from '../../assets/Aya.jpg';
+import profilePic from '../../assets/Pictures/aya-full.png';
 
 function Bio() {
 
@@ -10,10 +10,7 @@ function Bio() {
                     <h2 className='bio__header'>Hello!</h2>
                     <div className='bio__header--box'></div>
                 </div>
-                <h3 className='bio__subheader'>A positive and focused Software Developer with years of management work experience and a math background. </h3>
-                <p className='bio__p'>
-                    I have always found joy and satisfaction in logic, coding, and collaboration, so transitioning to software development just made sense. I earned two Bachelor Degrees (B.S. and B.A.) from The University of Texas, double-majoring in Math and Theatre. I loved the juxtaposition of math and art -- math and science provided joy in working out concrete solutions, while art provided freedom due to the absence of such concrete solutions. After university, I began my career as a software implementation Project Manager at Epic, and found myself increasingly intrigued by the intricacies of developing effective software that solves problems. I also grew my analytical and communication skills, which I actively bring into the software development process. Combining attention to detail, problem-solving, and organizational skills gained from coordinating a non-profit tutoring center, I have become a driven and nuanced Developer.
-                </p>
+                <p className='bio__p'>My name is Aya and I'm a positive and detail-oriented Full-Stack Software Developer living in Vancouver, BC. </p>
             </div>
             <img className='bio__pic' src={profilePic} alt='headshot'/>
         </div>
--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,5 +1,7 @@
 import './Projects.scss';
-import profilePic from '../../assets/Aya.jpg';
+import profilePic from '../../assets/Pictures/aya-full.png';
+import sudotwoLaptop from '../../assets/Pictures/sudotwo-laptop.png';
+import sudotwoPhone from '../../assets/Pictures/sudotwo-phone.png';
 
 
 function Projects() {
@@ -9,20 +11,28 @@ function Projects() {
             <h2 className='projects__title'>Projects</h2>
             <div className='projects__section'>
                 <div className='card'>
-                    <img className='card__img' src={profilePic} alt='project 1'/>
+                    <div className='card__images'>
+                        <img className='card__phone' src={sudotwoPhone} alt='sudotwo'/>
+                        <img className='card__laptop' src={sudotwoLaptop} alt='sudotwo'/>
+                    </div>
                     <div className='card__text'>
-                        <h3 className='card__title'>Project 1</h3>
+                        <h3 className='card__title'>
+                            <a className='card__link' href='https://sudotwo.com/'>Sudotwo!</a>
+                        </h3>
                         <div className='card__tags'>
                             <span className='card__tag'>JavaScript</span>
+                            <span className='card__tag'>React</span>
+                            <span className='card__tag'>Express</span>
+                            <span className='card__tag'>Node.js</span>
                             <span className='card__tag'>HTML</span>
                             <span className='card__tag'>CSS</span>
-                            <span className='card__tag'>Express</span>
+                            <span className='card__tag'>Socket.io</span>
                         </div>
-                        <p className='card__p'>Here's some info about the project</p>
+                        <p className='card__p'>I planned and developed this mobile-first multiplayer and multi-room sudoku gaming site</p>
                     </div>
                 </div>
                 <div className='card'>
-                    <img className='card__img' src={profilePic} alt='project 2'/>
+                    {/* <img className='card__img' src={sudotwo} alt='project 2'/> */}
                     <div className='card__text'>
                         <h3 className='card__title'>Project 2</h3>
                         <div className='card__tags'>
@@ -35,7 +45,7 @@ function Projects() {
                     </div>
                 </div>
                 <div className='card'>
-                    <img className='card__img' src={profilePic} alt='project 3'/>
+                    {/* <img className='card__img' src={profilePic} alt='project 3'/> */}
                     <div className='card__text'>
                         <h3 className='card__title'>Project 3</h3>
                         <div className='card__tags'>
