104b515f1f4df616a092f262047254ba5c0a1328
Aya Seidemann <aya.seidemann@gmail.com>
2023-01-28 13:46:13 -0800

    add tablet bio boxes

--- src/components/Bio/Bio.js
+++ src/components/Bio/Bio.js
@@ -8,11 +8,12 @@ function Bio() {
             <div className='bio__text'>
                 <div className='bio__header--wrapper'>
                     <h2 className='bio__header'>Hello!</h2>
-                    <div className='bio__header--box'></div>
                 </div>
                 <p className='bio__p'>My name is Aya and I'm a positive and detail-oriented Full-Stack Software Developer living in Vancouver, BC. </p>
+                <div className='bio__text--box'></div>
             </div>
             <img className='bio__pic' src={profilePic} alt='headshot'/>
+            <div className='bio__pic--box'></div>
         </div>
     )
 }
