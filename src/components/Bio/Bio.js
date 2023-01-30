ac4ce990d4dad22605e19d9d59c8b537a46dc91c
Aya Seidemann <aya.seidemann@gmail.com>
2023-01-30 11:27:31 -0800

    redo bio component, add icons + scroll

--- src/components/Bio/Bio.js
+++ src/components/Bio/Bio.js
@@ -1,21 +1,82 @@
-// import './Bio.scss';
-// import profilePic from '../../assets/Pictures/aya-full.png';
+import './Bio.scss';
+import profilePic from '../../assets/Pictures/aya-full.png';
 
-// function Bio() {
+// icons
+import envelopeMediumDark from '../../assets/Icons/envelope-medium-dark.png';
+import envelopeMediumLight from '../../assets/Icons/envelope-medium-light.png';
+import githubMediumDark from '../../assets/Icons/github-medium-dark.png';
+import githubMediumLight from '../../assets/Icons/github-medium-light.png';
+import linkedinMediumDark from '../../assets/Icons/linkedin-medium-dark.png';
+import linkedinMediumLight from '../../assets/Icons/linkedin-medium-light.png';
 
-//     // return (
-//     //     <div className='bio'>
-//     //         <div className='bio__text'>
-//     //             <div className='bio__header--wrapper'>
-//     //                 <h2 className='bio__header'>Hello!</h2>
-//     //             </div>
-//     //             <p className='bio__p'>My name is Aya and I'm a positive and detail-oriented Full-Stack Software Developer living in Vancouver, BC. </p>
-//     //             <div className='bio__text--box'></div>
-//     //         </div>
-//     //         <img className='bio__pic' src={profilePic} alt='headshot'/>
-//     //         <div className='bio__pic--box'></div>
-//     //     </div>
-//     // )
-// }
+function Bio({ mode }) {
 
-// export default Bio;
+    // get icons to show and hide based on window position
+    let bioIcons = document.querySelector('.bio__icons');
+    function iconDisplay() {
+        if (window.scrollY > 200) {
+            bioIcons.classList.add('bio__icons--hidden');
+        } else {
+            bioIcons.classList.remove('bio__icons--hidden');
+        }
+    }
+    window.addEventListener('scroll', iconDisplay);
+
+    return (
+        <div className='bio'>
+            <div className='bio__text'>
+                <h2 className='bio__header'>Hello!</h2>
+                <p className='bio__p'>My name is Aya and I'm a positive and detail-oriented Full-Stack Software Developer living in Vancouver, BC. </p>
+                <div className='bio__text--box'></div>
+            </div>
+            <img className='bio__pic' src={profilePic} alt='headshot' />
+            <div className='bio__pic--box'></div>
+            {mode === 'light' &&
+                        <div className='bio__icons'>
+                            <a className='bio__link bio__link--email' href="mailto:aya.seidemann@gmail.com">
+                                <img
+                                    className='bio__icon bio__icon--email'
+                                    src={envelopeMediumDark}
+                                    alt='email' />
+                            </a>
+                            <a className='bio__link bio__link--github' href='https://github.com/ayaseidemann'>
+                                <img
+                                    className='bio__icon bio__icon--github'
+                                    src={githubMediumDark}
+                                    alt='github' />
+                            </a>
+                            <a className='bio__link bio__link--linkedin' href='https://www.linkedin.com/in/aya-seidemann/'>
+                                <img
+                                    className='bio__icon bio__icon--linkedin'
+                                    src={linkedinMediumDark}
+                                    alt='linkedin' />
+                            </a>
+                        </div>
+                    }
+                    {mode === 'dark' &&
+                        <div className='bio__icons'>
+                            <a className='bio__link bio__link--email' href="mailto:aya.seidemann@gmail.com">
+                                <img
+                                    className='bio__icon bio__icon--email'
+                                    src={envelopeMediumLight}
+                                    alt='email' />
+                            </a>
+                            <a className='bio__link bio__link--github' href='https://github.com/ayaseidemann'>
+                                <img
+                                    className='bio__icon bio__icon--github'
+                                    src={githubMediumLight}
+                                    alt='github' />
+                            </a>
+                            <a className='bio__link bio__link--linkedin' href='https://www.linkedin.com/in/aya-seidemann/'>
+                                <img
+                                    className='bio__icon bio__icon--linkedin'
+                                    src={linkedinMediumLight}
+                                    alt='linkedin' />
+                            </a>
+                        </div>
+                    }
+        </div>
+    )
+}
+
+export default Bio;
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,6 @@
 import './Header.scss';
 import { Link } from 'react-router-dom';
 
-// photo
-import profilePic from '../../assets/Pictures/aya-full.png';
-
 // icons
 import moonMediumDark from '../../assets/Icons/moon-medium-dark.png';
 import sunMediumLight from '../../assets/Icons/sun-medium-light.png';
@@ -17,81 +14,70 @@ import linkedinMediumLight from '../../assets/Icons/linkedin-medium-light.png';
 
 function Header({ mode, toggleDarkMode }) {
 
-    return(
-        <>
-            <div className='header__wrapper'>
-                <header className='header'>
-                    <Link className='header__home-link' to='/'>
-                        <h1 className='header__title'>Aya Seidemann</h1>
-                    </Link>
-                    <div className='header__right'>
-                        {mode === 'light' &&
+    return (
+        <div className='header__wrapper'>
+            <header className='header'>
+                <Link className='header__home-link' to='/'>
+                    <h1 className='header__title'>Aya Seidemann</h1>
+                </Link>
+                <div className='header__right'>
+                    {mode === 'light' &&
                         <>
-                            <img 
+                            <img
                                 className='header__icon header__icon--mode'
                                 src={moonMediumDark}
                                 onClick={() => toggleDarkMode()}
-                                alt='darkmode'/>
+                                alt='darkmode' />
                             <a className='header__link header__link--email' href="mailto:aya.seidemann@gmail.com">
-                                <img 
-                                    className='header__icon header__icon--email' 
+                                <img
+                                    className='header__icon header__icon--email'
                                     src={envelopeMediumDark}
-                                    alt='email'/>
+                                    alt='email' />
                             </a>
                             <a className='header__link header__link--github' href='https://github.com/ayaseidemann'>
-                                <img 
-                                    className='header__icon header__icon--github' 
+                                <img
+                                    className='header__icon header__icon--github'
                                     src={githubMediumDark}
-                                    alt='github'/>
+                                    alt='github' />
                             </a>
                             <a className='header__link header__link--linkedin' href='https://www.linkedin.com/in/aya-seidemann/'>
-                                <img 
-                                    className='header__icon header__icon--linkedin' 
+                                <img
+                                    className='header__icon header__icon--linkedin'
                                     src={linkedinMediumDark}
-                                    alt='linkedin'/>
+                                    alt='linkedin' />
                             </a>
                         </>
-                        }
-                        {mode === 'dark' &&
+                    }
+                    {mode === 'dark' &&
                         <>
-                            <img 
+                            <img
                                 className='header__icon header__icon--mode'
                                 src={sunMediumLight}
                                 onClick={() => toggleDarkMode()}
-                                alt='darkmode'/>
+                                alt='darkmode' />
                             <a className='header__link header__link--email' href="mailto:aya.seidemann@gmail.com">
-                                <img 
-                                    className='header__icon header__icon--email' 
+                                <img
+                                    className='header__icon header__icon--email'
                                     src={envelopeMediumLight}
-                                    alt='email'/>
+                                    alt='email' />
                             </a>
                             <a className='header__link header__link--github' href='https://github.com/ayaseidemann'>
-                                <img 
-                                    className='header__icon header__icon--github' 
+                                <img
+                                    className='header__icon header__icon--github'
                                     src={githubMediumLight}
-                                    alt='github'/>
+                                    alt='github' />
                             </a>
                             <a className='header__link header__link--linkedin' href='https://www.linkedin.com/in/aya-seidemann/'>
-                                <img 
-                                    className='header__icon header__icon--linkedin' 
+                                <img
+                                    className='header__icon header__icon--linkedin'
                                     src={linkedinMediumLight}
-                                    alt='linkedin'/>
+                                    alt='linkedin' />
                             </a>
                         </>
-                        }
-                    </div>
-                </header>
-            </div>
-            <div className='bio'>
-                <div className='bio__text'>
-                    <h2 className='bio__header'>Hello!</h2>
-                    <p className='bio__p'>My name is Aya and I'm a positive and detail-oriented Full-Stack Software Developer living in Vancouver, BC. </p>
-                    <div className='bio__text--box'></div>
+                    }
                 </div>
-                <img className='bio__pic' src={profilePic} alt='headshot'/>
-                <div className='bio__pic--box'></div>
-            </div>
-        </>
+            </header>
+        </div>
     )
 }
 
